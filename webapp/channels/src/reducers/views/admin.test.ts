beae7da1ebc140bed93b904bc35d72d20d328b7b
Scott Bishel <scott.bishel@mattermost.com>
2024-11-05 16:42:18 -0700

    Implement storeing Team and Channel state

--- webapp/channels/src/reducers/views/admin.test.ts
+++ webapp/channels/src/reducers/views/admin.test.ts
@@ -1,9 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// import {adminConsoleTeamManagementTablePropertiesInitialState, adminConsoleChannelManagementTablePropertiesInitialState, adminConsoleUserManagementTablePropertiesInitialState} from 'reducers/views/admin';
+
 import {ActionTypes} from 'utils/constants';
 
-import {needsLoggedInLimitReachedCheck} from './admin';
+import {needsLoggedInLimitReachedCheck, adminConsoleTeamManagementTableProperties, adminConsoleChannelManagementTableProperties, adminConsoleUsersManagementTableProperties, 
+    adminConsoleTeamManagementTablePropertiesInitialState, adminConsoleChannelManagementTablePropertiesInitialState, adminConsoleUserManagementTablePropertiesInitialState} from './admin';
 
 describe('views/admin reducers', () => {
     describe('needsLoggedInLimitReachedCheck', () => {
@@ -24,6 +27,42 @@ describe('views/admin reducers', () => {
                 {type: ActionTypes.NEEDS_LOGGED_IN_LIMIT_REACHED_CHECK, data: true},
             );
             expect(trueValue).toBe(true);
+        });
+    });
+
+    describe('team reducer', () => {
+        const newState = {
+            pageIndex: 2,
+            searchTerms: 'hello',
+            searchOpts: {},
+        };
+
+        test('set state to new state', () => {
+            const actual = adminConsoleTeamManagementTableProperties(adminConsoleTeamManagementTablePropertiesInitialState, {type: ActionTypes.SET_ADMIN_CONSOLE_TEAM_MANAGEMENT_TABLE_PROPERTIES, data: newState});
+            expect(actual).toBe(newState);
+        });
+
+        test('clear state', () => {
+            const actual = adminConsoleTeamManagementTableProperties(adminConsoleTeamManagementTablePropertiesInitialState, {type: ActionTypes.CLEAR_ADMIN_CONSOLE_TEAM_MANAGEMENT_TABLE_PROPERTIES, data: newState});
+            expect(actual).toBe(adminConsoleTeamManagementTablePropertiesInitialState);
+        });
+    });
+
+    describe('channel reducer', () => {
+        const newState = {
+            pageIndex: 2,
+            searchTerms: 'hello',
+            searchOpts: {},
+        };
+
+        test('set state to new state', () => {
+            const actual = adminConsoleChannelManagementTableProperties(adminConsoleChannelManagementTablePropertiesInitialState, {type: ActionTypes.SET_ADMIN_CONSOLE_CHANNEL_MANAGEMENT_TABLE_PROPERTIES, data: newState});
+            expect(actual).toBe(newState);
+        });
+
+        test('clear state', () => {
+            const actual = adminConsoleChannelManagementTableProperties(adminConsoleChannelManagementTablePropertiesInitialState, {type: ActionTypes.CLEAR_ADMIN_CONSOLE_CHANNEL_MANAGEMENT_TABLE_PROPERTIES, data: newState});
+            expect(actual).toBe(adminConsoleChannelManagementTablePropertiesInitialState);
         });
     });
 });
